19e41594f63118fcc4351cc5044b66128515e733
Erik Golden <erikgolden7@gmail.com>
2017-03-01 16:08:02 -0700

    fixed forecast and box spacing

--- db/createUserBadge.sql
+++ db/createUserBadge.sql
@@ -1,4 +1,3 @@
-insert into badges
-(user_name, user_id, html_badge, basic_js_badge, int_js_badge, angular_badge, node_badge, sql_badge, pproj_badge, gproj_badge)
-values ($1, $2, true, false, false, false, false, false, false, false)
-returning user_name, user_id, true, false, false, false, false, false, false, false;
+INSERT INTO users (user_name, user_id, html_badge, basic_js_badge, int_js_badge, angular_badge, node_badge, sql_badge, pproj_badge, gproj_badge)
+VALUES ($1, $2, true, true, true, false, false, false, false, false)
+WHERE users.id = $3;
